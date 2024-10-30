63f97cd0147137eda594ddebb0cb9939ebe57da3
Jonathan S Reisgen <72268552+jreisgen@users.noreply.github.com>
2024-10-31 00:17:39 +0100

    Delete Week 2 directory to merge as branch later

--- Week 2/L2E2.py
+++
@@ -1,8 +0,0 @@
-'''Exercise 2: Basic Data Filtering
-1. Create a List of Mixed Data Types: Create a list that contains a mix of integers, strings, and floats.
-2. Filter the List: Use list comprehension to create a new list that contains only the integers from the original list.
-3. Print the New List: Output the filtered list of integers.'''
-
-mixedList = [3, "string", 4.21, "string", 3.12, 4]
-ints = [i for i in mixedList if type(i)== int]
-print(ints)
